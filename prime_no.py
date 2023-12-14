f6369076148f7a9bc0d4c571eb2ae3c8a40c5398
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2023-12-14 14:19:19 +0530

    Update prime_no.py

--- prime_no.py
+++ prime_no.py
@@ -1,7 +1,7 @@
 num = int(input("Enter any Integer:"))
 n = int((num/2)+1)
-if (num == 0 or num == 1):
-    print("Prime Number is not defined for 0 and 1")
+if (num == 0 or num == 1 or num < 0):
+    print("Prime Number is not defined for 0 and 1 and also for negative values")
 else:
     for i in range(int(n/2 + 1), 2, -1):
         if num % i == 0:
