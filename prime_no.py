41f3f73705bcc52cb8a44be100f5d6d78d1895df
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2023-12-14 14:11:35 +0530

    Update prime_no.py

--- prime_no.py
+++ prime_no.py
@@ -3,7 +3,7 @@ n=int((num/2)+1)
 if (num==0 or num==1):
     print("Prime Number is not defined for 0 and 1")
 else:
-    for i in range(n,2,-1):
+    for i in range(int(n/2 + 1),2,-1):
         if num%i==0:
             print(num,"is not Prime number")
             break
